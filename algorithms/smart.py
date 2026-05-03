8c48b46109de9fe042c03ba59b9550a59664c1a9
basicesraa <116526974+basicesraa@users.noreply.github.com>
2026-05-03 09:36:46 +0300

    Update smart.py

--- algorithms/smart.py
+++ algorithms/smart.py
@@ -14,14 +14,11 @@ def smart_algorithm(queues, time):
         except:
             total_wait = 0
 
-        # core logic
         score[d] = queue_len + 0.5 * total_wait
 
-        # fairness
         if d == last_direction:
             score[d] -= 2
 
-        # anti-starvation
         if total_wait > 20:
             score[d] += 10
 
